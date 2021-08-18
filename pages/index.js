e66d87c4e8a092445bd59fe3c952074b0c303498
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-18 23:54:47 +0530

    update-home-page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
 
 const HomePage = () => {
 
@@ -7,7 +8,7 @@ const HomePage = () => {
 
     return (
         <div>
-            <h1>The Home Page</h1>
+            <EventList items={featuredEvents} />
         </div>
     )
 }
