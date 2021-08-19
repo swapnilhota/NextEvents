bb47f03e4cc2ab80fb6b171277a6466d8a70f1eb
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-19 12:44:18 +0530

    update-event-detail

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Fragment } from 'react';
 import { useRouter } from 'next/router'
 import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 const EventDetailPage = () => {
 
@@ -14,9 +18,13 @@ const EventDetailPage = () => {
     }
 
     return (
-        <div>
-            <h1>Event Detail</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     )
 }
 
