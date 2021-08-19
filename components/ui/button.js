153263c20861e3abd90e75cf417b1fc7d0199aba
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-19 12:04:54 +0530

    create-btn-comp

---
+++ components/ui/button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link';
+
+const Button = (props) => {
+    return (
+        <Link href={props.link}>{props.children}</Link>
+    )
+}
+
+export default Button
