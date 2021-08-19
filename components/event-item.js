de484c28d658ad9bbfa1d3a3c673bb10f095438b
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-19 12:19:16 +0530

    update-event-item-comp

--- components/event-item.js
+++ components/event-item.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import Link from 'next/link';
 import classes from './event-item.module.css';
 import Button from './ui/button';
+import DateIcon from './icons/date-icon';
+import AddressIcon from './icons/address-icon';
+import ArrowRightIcon from './icons/arrow-right-icon';
 
 const EventItem = (props) => {
 
@@ -24,14 +26,19 @@ const EventItem = (props) => {
                 <div className={classes.summary}>
                     <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{humanReadableDate}</time>
                     </div>
                     <div className={classes.address}>
+                        <AddressIcon />
                         <address>{formattedAddress}</address>
                     </div>
                 </div>
                 <div className={classes.action}>
-                    <Button link={exploreLink}>Explore Event</Button>
+                    <Button link={exploreLink}>
+                        <span>Explore Event</span>
+                        <span className={classes.icon}><ArrowRightIcon /></span>
+                    </Button>
                 </div>
             </div>
         </li>
