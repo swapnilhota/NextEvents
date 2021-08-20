1cb1a5fd0b251040c35e2cbece256cad31f8c086
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-20 17:47:23 +0530

    update-events-search

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Button from '../ui/button';
+import classes from './events-search.module.css';
 
 const EventsSearch = (props) => {
     return (
-        <form action="">
-            <div>
-                <div>
+        <form className={classes.form}>
+            <div className={classes.controls}>
+                <div className={classes.control}>
                     <label htmlFor="year">Year</label>
                     <select name="" id="year">
                         <option value="2021">2021</option>
                         <option value="2022">2022</option>
                     </select>
                 </div>
-                <div>
+                <div className={classes.control}>
                     <label htmlFor="month">Month</label>
                     <select name="" id="month">
                         <option value="1">Jan</option>
