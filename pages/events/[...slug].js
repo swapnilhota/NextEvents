6e958740a60d7cde174aade609496497aa745034
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-20 18:09:42 +0530

    update-filteredEventsPage

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,12 +1,40 @@
 import React from 'react'
 import { useRouter } from 'next/dist/client/router'
+import { getFilteredEvents } from '../../dummy-data';
 
 const FilteredEventsPage = () => {
 
     const router = useRouter();
     const filterData = router.query.slug;
+
+    if (!filterData) {
+        return (
+            <p className="center">Loading...</p>
+        )
+    }
+
     const year = filterData[0];
     const month = filterData[1];
+
+    const numYear = +year;
+    const numMonth = +month;
+
+    if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+        return (
+            <p>Invalid Filters</p>
+        )
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth
+    })
+
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return (
+            <p>No events found for chosen filter</p>
+        )
+    }
 
     return (
         <div>
