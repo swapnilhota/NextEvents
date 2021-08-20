ee2340733ef72b1e46db9727f5e7cb5e29765f96
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-20 18:00:04 +0530

    update-all-events

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
+import { useRouter } from 'next/dist/client/router';
 
 const AllEventsPage = () => {
 
     const events = getAllEvents();
+    const router = useRouter();
+
+    const findEventsHandler = (year, month) => {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath); //programmatic navigation
+    }
 
     return (
         <div>
-            <EventsSearch />
+            <EventsSearch onSearch={findEventsHandler} />
             <EventList items={events} />
         </div>
     )
