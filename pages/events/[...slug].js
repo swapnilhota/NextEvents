cd41e6e050459f9c48f06d0e6856a466460836a7
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-20 18:04:44 +0530

    update-global-css

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useRouter } from 'next/dist/client/router'
 
 const FilteredEventsPage = () => {
+
+    const router = useRouter();
+    const filterData = router.query.slug;
+    const year = filterData[0];
+    const month = filterData[1];
+
     return (
         <div>
             <h1>Filtered Events</h1>
