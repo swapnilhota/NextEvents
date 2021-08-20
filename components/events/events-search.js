c083427b585a3c3ccae9d61f9faa2c8be8528f5a
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-20 17:53:56 +0530

    update-events-search

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import Button from '../ui/button';
 import classes from './events-search.module.css';
+import { useRef } from 'react';
 
 const EventsSearch = (props) => {
+
+    const yearInputRef = useRef();
+    const monthInputRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const selectedYear = yearInputRef.current.value;
+        const selectedMonth = monthInputRef.current.value;
+
+        props.onSearch(selectedYear, selectedMonth);
+    }
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.controls}>
                 <div className={classes.control}>
                     <label htmlFor="year">Year</label>
-                    <select name="" id="year">
+                    <select name="" id="year" ref={yearInputRef}>
                         <option value="2021">2021</option>
                         <option value="2022">2022</option>
                     </select>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="month">Month</label>
-                    <select name="" id="month">
+                    <select name="" id="month" ref={monthInputRef}>
                         <option value="1">Jan</option>
                         <option value="2">Feb</option>
                         <option value="3">Mar</option>
